733959183a9d5fda3ac17ecb8f292a9f5deef009
Matthew Cullen <m23.cullen@gmail.com>
2021-02-11 17:48:10 -0500

    created file and base of Employee Model and Controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Models;
+
+namespace SqliteFromScratch.Controllers 
+{
+  [Route("api/[Controller]")]
+  public class EmployeeController : Controller 
+  {
+
+  }
+}
